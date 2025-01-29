f918f39dcffdb32db2befdc821e215de86583745
redondo519 <david.redalo@iesjulianmarias.es>
2025-01-29 20:36:05 +0100

    comit

---
+++ 9POO/src/Libro.java
@@ -0,0 +1,37 @@
+public class Libro {
+    private String isbn;
+    private String titulo;
+    private String autor;
+
+    public Libro(String isbn, String titulo, String autor) {
+        this.isbn = isbn;
+        this.titulo = titulo;
+        this.autor = autor;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+
+}
