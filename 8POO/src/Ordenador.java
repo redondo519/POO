b5777639f1669e1ba90cb839c493b80fac5853c2
redondo519 <david.redalo@iesjulianmarias.es>
2025-01-29 19:22:59 +0100

    comit

---
+++ 8POO/src/Ordenador.java
@@ -0,0 +1,39 @@
+public class Ordenador {
+
+    private String modeloCPU;
+    private int tamDISC;
+    private int tamRAM;
+    private double precio;
+
+    public String getModeloCPU() {
+        return modeloCPU;
+    }
+
+    public void setModeloCPU(String modeloCPU) {
+        this.modeloCPU = modeloCPU;
+    }
+
+    public int getTamDISC() {
+        return tamDISC;
+    }
+
+    public void setTamDISC(int tamDISC) {
+        this.tamDISC = tamDISC;
+    }
+
+    public int getTamRAM() {
+        return tamRAM;
+    }
+
+    public void setTamRAM(int tamRAM) {
+        this.tamRAM = tamRAM;
+    }
+
+    public double getPrecio() {
+        return precio;
+    }
+
+    public void setPrecio(double precio) {
+        this.precio = precio;
+    }
+}
